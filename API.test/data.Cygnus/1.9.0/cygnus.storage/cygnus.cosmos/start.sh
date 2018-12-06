c7fbc3888665cceadb95ee260f70e68ee31c3fb3
Pasquale Vitale <bcdbc3989a08de949de4c0fa7b0cc0c25c6c765d>
2018-12-06 16:48:38 +0100

    Delete test cygnus 1.9.0 with cosmos

--- API.test/data.Cygnus/1.9.0/cygnus.storage/cygnus.cosmos/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-echo "Start Cygnus"
-export FLUME_HOME=/usr/cygnus
-$FLUME_HOME/bin/cygnus-flume-ng agent --conf $FLUME_HOME/conf -f $FLUME_HOME/conf/agent_ngsi_cosmos.conf -n cygnus-ngsi -p 5080 -Dflume.root.logger=INFO,console,LOGFILE -Duser.timezone=UTC
-
-
-
